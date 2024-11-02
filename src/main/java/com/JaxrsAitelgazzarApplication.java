f12e8a85b39698c88292fd0a181c48a3f61f6657
Aitelgazzar <mohamedaitelgazzar@gmail.com>
2024-11-02 15:49:29 +0100

    v1

---
+++ src/main/java/com/JaxrsAitelgazzarApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxrsAitelgazzarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaxrsAitelgazzarApplication.class, args);
+    }
+
+}
